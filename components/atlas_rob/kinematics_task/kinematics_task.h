8a07c689ac0871b78d92f7fa0d81c2bb42b5d45e
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-07 21:03:24 +0200

    a

--- components/atlas_rob/kinematics_task/kinematics_task.h
+++
@@ -1,9 +0,0 @@
-#ifndef KINEMATICS_TASK_KINEMATICS_TASK_H
-#define KINEMATICS_TASK_KINEMATICS_TASK_H
-
-#include "common.h"
-
-void kinematics_task_initialize(void);
-void kinematics_queue_initialize(void);
-
-#endif // KINEMATICS_TASK_KINEMATICS_TASK_H
