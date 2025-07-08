3ebe46525db5068a90840cfcb847446e051baef6
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-08 16:41:58 +0200

    changes

--- components/atlas_rob/atlas_rob.c
+++ components/atlas_rob/atlas_rob.c
@@ -5,6 +5,7 @@
 #include "packet_task.h"
 #include "system_task.h"
 #include "tim.h"
+#include "uart_config.h"
 #include "uart_task.h"
 #include "usart.h"
 
@@ -36,9 +37,10 @@ static void atlas_rob_uart_initialize(void)
     static StackType_t uart_task_stack[UART_TASK_STACK_DEPTH];
     static uint8_t uart_buffer[UART_BUFFER_STORAGE_SIZE];
 
-    static uart_task_ctx_t task_ctx = {};
+    static uart_task_ctx_t task_ctx;
     task_ctx.stream_buffer = stream_buffer;
-    task_ctx.uart = &huart2, task_ctx.uart_action = UART_ACTION_TRANSMIT;
+    task_ctx.uart = &huart2;
+    task_ctx.uart_action = UART_ACTION_TRANSMIT;
     task_ctx.uart_buffer = uart_buffer;
     task_ctx.uart_buffer_size = UART_BUFFER_STORAGE_SIZE;
 
@@ -50,6 +52,30 @@ static void atlas_rob_uart_initialize(void)
                                           UART_TASK_PRIORITY,
                                           UART_TASK_STACK_DEPTH,
                                           uart_task_stack));
+}
+
+void uart_receive_complete_callback(void)
+{
+    BaseType_t task_woken = pdFALSE;
+
+    xTaskNotifyFromISR(task_manager_get(TASK_TYPE_UART),
+                       UART_NOTIFY_RECEIVE_COMPLETE,
+                       eSetBits,
+                       &task_woken);
+
+    portYIELD_FROM_ISR(task_woken);
+}
+
+void uart_transmit_complete_callback(void)
+{
+    BaseType_t task_woken = pdFALSE;
+
+    xTaskNotifyFromISR(task_manager_get(TASK_TYPE_UART),
+                       UART_NOTIFY_TRANSMIT_COMPLETE,
+                       eSetBits,
+                       &task_woken);
+
+    portYIELD_FROM_ISR(task_woken);
 }
 
 static void atlas_rob_joints_initialize(void)
